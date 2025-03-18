b49f84fd3abaf65af885b03b5b0a10199dc3c91f
julio20212 <162452155+Julio20212@users.noreply.github.com>
2025-03-18 11:39:34 -0300

    Add files via upload

---
+++ DrogariaRepository.java
@@ -0,0 +1,9 @@
+package com.farmacia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farmacia.entity.Drogaria;
+
+public interface DrogariaRepository extends JpaRepository <Drogaria, Long> {
+
+}
